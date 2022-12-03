4e0f68f50b90976358549059dee5413b46e3d63d
Sungwook Choi <sungwook.csw@gmail.com>
2022-12-03 22:57:09 +0900

    feat add navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <a href="/" className="site-title">
+        bz-react-hook-form-practice
+      </a>
+      <ul>
+        <li>
+          <a href="login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
