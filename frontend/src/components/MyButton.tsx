cad9228b926aa0c25d9966ef00cba90cfedd40b8
Sungwook Choi <sungwook.csw@gmail.com>
2023-01-07 11:48:52 +0900

    add types

--- frontend/src/components/MyButton.tsx
+++ frontend/src/components/MyButton.tsx
@@ -1,5 +1,11 @@
-const MyButton = ({ text, color }) => {
-  return <button type="button">{text}</button>;
+type PropTypes = { text: string; color: string };
+
+const MyButton = ({ text, color }: PropTypes) => {
+  return (
+    <button type="button" color={color}>
+      {text}
+    </button>
+  );
 };
 
 export default MyButton;
