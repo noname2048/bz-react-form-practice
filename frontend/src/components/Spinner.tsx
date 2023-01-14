6c475d43372215ea53318b9787eb6ea09006cb56
Sungwook Choi <sungwook.csw@gmail.com>
2023-01-14 23:12:18 +0900

    refactor: refactor spinner

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -4,20 +4,43 @@ import styled from "styled-components";
 const Spinner = (props: {
   spinnerSize: string;
   color: string;
+  pointColor: string;
   text: string;
 }) => {
-  const { spinnerSize, color, text } = props;
-  return <Circle spinnerSize={spinnerSize} color={color} text={text} />;
+  const {
+    spinnerSize = "",
+    color = "#535bf2",
+    pointColor = "#213547",
+    text = "",
+  } = props;
+
+  let calculatedSpinnerSize = "30px";
+  if (text) calculatedSpinnerSize = "20px";
+
+  return (
+    <Container>
+      <Circle
+        spinnerSize={spinnerSize || calculatedSpinnerSize}
+        color={color}
+        pointColor={pointColor}
+      />
+      <p>text</p>
+    </Container>
+  );
 };
 
 export default Spinner;
 
-const Circle = styled.div<{}>`
+const Circle = styled.div<{
+  spinnerSize: string;
+  color: string;
+  pointColor: string;
+}>`
+  width: ${(props) => props.spinnerSize};
+  height: ${(props) => props.spinnerSize};
   border-radius: 50%;
-  border: 4px solid #535bf2;
-  width: 30px;
-  height: 30px;
-  border-top: 4px solid #213547;
+  border: 4px solid ${({ color }) => color};
+  border-top: 4px solid ${({ pointColor }) => pointColor};
 
   animation: 1s infinite linear rotate;
   @keyframes rotate {
@@ -28,4 +51,12 @@ const Circle = styled.div<{}>`
       transform: rotate(360deg);
     }
   }
+`;
+
+const Container = styled.div<{}>`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
 `;
