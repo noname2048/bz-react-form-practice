a7e165e2035121370f6e9e78f008f5bb35882f64
Sungwook Choi <sungwook.csw@gmail.com>
2023-01-25 23:54:12 +0900

    feat: persist reordering

--- frontend/src/components/domain/todo/TodoIndex.jsx
+++ frontend/src/components/domain/todo/TodoIndex.jsx
@@ -6,7 +6,36 @@ import { DragDropContext } from "react-beautiful-dnd";
 const TodoIndex = () => {
   const [state, setState] = useState(initialData);
 
-  const onDragEnd = (result) => null;
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColum = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: { ...state.columns, [newColum.id]: newColum },
+    };
+    setState(newState);
+  };
   return (
     <div>
       <DragDropContext onDragEnd={onDragEnd}>
