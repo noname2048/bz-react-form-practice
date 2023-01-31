180a9fb17186d616c51440932ff7b886d4899fe1
Sungwook Choi <sungwook.csw@gmail.com>
2023-02-01 00:36:06 +0900

    feat: add columns and can moving around

--- frontend/src/components/domain/todo/TodoIndex.jsx
+++ frontend/src/components/domain/todo/TodoIndex.jsx
@@ -1,7 +1,12 @@
 import initialData from "./data.js";
 import { useState } from "react";
 import Column from "./Column";
+import styled from "styled-components";
 import { DragDropContext } from "react-beautiful-dnd";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const TodoIndex = () => {
   const [state, setState] = useState(initialData);
@@ -35,21 +40,51 @@ const TodoIndex = () => {
       return;
     }
 
-    const column = state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    const start = state.columns[source.droppableId];
+    const finish = state.columns[destination.droppableId];
 
-    const newColum = {
-      ...column,
-      taskIds: newTaskIds,
+    if (start === finish) {
+      const column = state.columns[source.droppableId];
+      const newTaskIds = Array.from(column.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColum = {
+        ...column,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...state,
+        columns: { ...state.columns, [newColum.id]: newColum },
+        // 무슨 문법인지 알아볼것 -> 계산된 속성이름 구조 분해 (computed property name)
+        // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+      };
+      setState(newState);
+      return;
+    }
+
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
     };
 
     const newState = {
       ...state,
-      columns: { ...state.columns, [newColum.id]: newColum },
-      // 무슨 문법인지 알아볼것 -> 계산된 속성이름 구조 분해 (computed property name)
-      // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+      columns: {
+        ...state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
     };
     setState(newState);
   };
@@ -60,11 +95,13 @@ const TodoIndex = () => {
         onDragStart={onDragStart}
         onDragUpdate={onDragUpdate}
       >
-        {state.columnOrder.map((columnId) => {
-          const column = state.columns[columnId];
-          const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
-          return <Column key={column.id} column={column} tasks={tasks} />;
-        })}
+        <Container>
+          {state.columnOrder.map((columnId) => {
+            const column = state.columns[columnId];
+            const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })}
+        </Container>
       </DragDropContext>
     </div>
   );
