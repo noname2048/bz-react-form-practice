2922267e0fa11b81fc0e778ca13c8cfbb1a41f3d
Sungwook Choi <sungwook.csw@gmail.com>
2023-02-13 23:58:57 +0900

    fix: fix types

--- frontend/src/components/domain/todo/dataTypes.tsx
+++ frontend/src/components/domain/todo/dataTypes.tsx
@@ -1,51 +1,61 @@
 export interface Task {
-    id: string,
-    content: string
+  id: string;
+  content: string;
 }
 
 export interface Column {
-    id: string,
-    title: string,
-    taskIds: Array<string>;
-}
-
-export interface InitData  {
-    tasks: {[id: string]: Task},
-    columns: {[id: string]: Column},
-    columnOrder: Array<string>
-}
-
-export interface DraggableLocation {
-    droppableId: string,
-    index: number,
-}
-
-export interface DroppableRubric {
-    draggableId: string,
-    type: string,
-    source: DraggableLocation,
-}
-
-// type 과 interface 의 차이를 알아볼것
-export type DragStart {
-    ...DroppableRubric
-}
-
-export interface OnDragStart {
-    ...DropableRubric,
-}
-
-export interface DragUpdate {
-    destination: DroppableRubric
-}
-
-export interface OnDragEnd {
-    source: DroppableRubric,
-    destination: DroppableRubric,
-    draggableId: string,
-    type: string
+  id: string;
+  title: string;
+  taskIds: Array<string>;
 }
 
 export interface InitData {
-
+  tasks: { [id: string]: Task };
+  columns: { [id: string]: Column };
+  columnOrder: Array<string>;
 }
+
+export interface DraggableLocation {
+  droppableId: string;
+  index: number;
+}
+
+export interface DroppableRubric {
+  draggableId: string;
+  type: string;
+  source: DraggableLocation;
+}
+
+enum MovementMode {
+  "FLUID",
+  "SNAP",
+}
+
+type MovementModeType = "FLUID" | "SNAP";
+
+// type 과 interface 의 차이를 알아볼것
+export interface DragStart extends DroppableRubric {
+  movementMode: MovementModeType;
+}
+
+export type DroppableRubricType = {
+  draggableId: string;
+  type: string;
+};
+
+export type DragStartType = DroppableRubricType & {
+  movementMode: MovementModeType;
+};
+
+export interface DragUpdate {
+  destination: DroppableRubric;
+}
+
+export interface OnDragEnd {
+  source: DroppableRubric;
+  destination: DroppableRubric;
+  draggableId: string;
+  type: string;
+}
+
+export interface InitData {}
