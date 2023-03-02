4075b4d601aa34394ab3c076f908192cece71479
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-02 23:19:22 +0900

    chore: add init data

--- kanban/src/kanban/atoms.ts
+++ kanban/src/kanban/atoms.ts
@@ -1,6 +1,30 @@
 import { atom } from "recoil";
 import { Kanban } from "./types";
 
+const init = [
+  {
+    id: "INTJ",
+    title: "todo list",
+    columns: [
+      {
+        title: "yesterday",
+        id: "yesterday",
+        tasks: [{ id: "refactor code", content: "refactor code" }],
+      },
+      {
+        title: "today",
+        id: "today",
+        tasks: [{ id: "get bug", content: "get bug" }],
+      },
+      {
+        title: "tomorrow",
+        id: "tomorrow",
+        tasks: [{ id: "get a job", content: "get a job" }],
+      },
+    ],
+  },
+];
+
 const kanbanState = atom({
   key: "kanbanState",
   default: [],
