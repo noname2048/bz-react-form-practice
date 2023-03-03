67669d04ef1ff78ee3a3b34debf24f7e5497a349
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-03 21:59:20 +0900

    fix: fix types of kanban

--- kanban/src/kanban/atoms.ts
+++ kanban/src/kanban/atoms.ts
@@ -1,30 +1,30 @@
 import { atom } from "recoil";
 import { Kanban } from "./types";
 
-const init = [
-  {
-    id: "INTJ",
-    title: "todo list",
-    columns: [
-      {
-        title: "yesterday",
-        id: "yesterday",
-        tasks: [{ id: "refactor code", content: "refactor code" }],
-      },
-      {
-        title: "today",
-        id: "today",
-        tasks: [{ id: "get bug", content: "get bug" }],
-      },
-      {
-        title: "tomorrow",
-        id: "tomorrow",
-        tasks: [{ id: "get a job", content: "get a job" }],
-      },
-    ],
-  },
-];
+const init: Kanban = {
+  id: "todo list",
+  title: "todo list",
+  columns: [
+    {
+      title: "yesterday",
+      id: "yesterday",
+      tasks: [
+        { id: "refactor code", content: "refactor code", is_done: false },
+      ],
+    },
+    {
+      title: "today",
+      id: "today",
+      tasks: [{ id: "get bug", content: "get bug", is_done: false }],
+    },
+    {
+      title: "tomorrow",
+      id: "tomorrow",
+      tasks: [{ id: "get a job", content: "get a job", is_done: false }],
+    },
+  ],
+};
 
-const kanbanState = atom<Kanban>(init);
+const kanbanState = atom<Kanban>({ key: "kanban", default: init });
 
 export default kanbanState;
