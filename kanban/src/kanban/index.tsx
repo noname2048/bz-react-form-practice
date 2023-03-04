561863b09597b71612ead85d0dfaa07062501db6
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-04 22:59:39 +0900

    refactor: add container

--- kanban/src/kanban/index.tsx
+++ kanban/src/kanban/index.tsx
@@ -1,10 +1,15 @@
 import { useRecoilState } from "recoil";
 import kanbanState from "./atoms";
 import {Kanban} from "./types";
+import styled from "styled-components"
 
 const KanbanPage = () => {
   const [kanban, setKanban] = useRecoilState<Kanban>(kanbanState);
-  return <>{kanban?.title}</>;
+  return <Container>{kanban?.title}</Container>;
 };
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
 export default KanbanPage;
