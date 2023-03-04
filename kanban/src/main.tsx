464319ac8c4755b4ff62d974507b2be59d695742
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-04 22:59:22 +0900

    feat: add recoil root

--- kanban/src/main.tsx
+++ kanban/src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import KanbanPage from "./kanban";
+import {RecoilRoot} from "recoil";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RecoilRoot>
+      <KanbanPage />
+    </RecoilRoot>
   </React.StrictMode>,
 )
