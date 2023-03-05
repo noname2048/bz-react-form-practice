773a3f376e848df8b1647d2f3d3472f0a5706ec7
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-05 23:54:06 +0900

    feat: apply global reset css

--- kanban/src/main.tsx
+++ kanban/src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import KanbanPage from "./kanban";
-import {RecoilRoot} from "recoil";
+import { RecoilRoot } from "recoil";
+import GlobalStyle from "./styles/GlobalStyle";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RecoilRoot>
       <KanbanPage />
     </RecoilRoot>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
